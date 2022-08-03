aad7e5005c445a247eba59bd28420f015296d57a
Yash Pradhan <96780365+YashPradhan2002@users.noreply.github.com>
2022-08-03 10:25:38 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 if (isset($_POST['submit'])) {
-$host ="localhost";
-$user = "root";
-$pass = "";
-$db = "phpfirst";
+$host ="ec2-107-22-122-106.compute-1.amazonaws.com";
+$user = "dzlomyfjmtqpeo";
+$pass = "2b91c439300cd1e66917df3fd6d7f1fa17ed136f6d77f8eee2dc269ab47e0e52";
+$db = "d718v4k9j2i0m8";
 $table = "registrations";
 $con = mysqli_connect($host, $user, $pass, $db);
 if ($con) {
